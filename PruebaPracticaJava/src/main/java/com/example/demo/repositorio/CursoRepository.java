9cb2bb7b2471ef1f3bdc74bf339a9c04bc14c6c1
Wilmer Garnica <wilmer.garnica@pragma.com.co>
2022-03-07 14:29:31 -0500

    cambio

--- PruebaPracticaJava/src/main/java/com/example/demo/repositorio/CursoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.models.Curso;
-
-public interface CursoRepository extends JpaRepository<Curso, Long> {
-
-}
